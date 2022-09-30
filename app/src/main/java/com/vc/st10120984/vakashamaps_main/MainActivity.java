1c7c89f7abf9234d23288a577f9603a4e42837dd
ganesh2106 <omeshann@gmail.com>
2022-09-30 11:52:21 +0200

    Main Branch with Fragments

---
+++ app/src/main/java/com/vc/st10120984/vakashamaps_main/MainActivity.java
@@ -0,0 +1,49 @@
+package com.vc.st10120984.vakashamaps_main;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.vc.st10120984.vakashamaps_main.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch(item.getItemId()){
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.savedPlaces:
+                    replaceFragment(new SavedPlacesragment());
+                    break;
+                case R.id.settings:;
+                    replaceFragment(new SettingsFragment());
+                break;
+
+            }
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+}
